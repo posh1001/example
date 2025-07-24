693f0ee2407e88c1f36a8f5ffd80abcbcef0003f
unknown <orifepaul30@gmail.com>
2025-07-24 08:37:52 -0700

    create an eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,39 +1,10 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-
-    public static function all(): array {
-
-           return [
-             [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => "$10,000"
-            ],
-
-             [
-                'id' => 2,
-                'title' => 'Digital Marketer',
-                'salary' => '$6,000'
-            ],
-
-             [
-                'id' => 3,
-                'title' => 'Content creator',
-                'salary' => '$3,000'
-             ]
-             ];
-
-    }
-
-    public static function find(int $id) {
-
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-    }
-
-};
+class Job extends Model
+{
+    protected $table = 'job_listing';
+}
