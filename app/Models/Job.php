08bc8df947068fd3e6c5d3e260c4ce8f1010ab40
unknown <orifepaul30@gmail.com>
2025-07-25 15:30:37 -0700

    creat migrations and populate table with data

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listing';
+
+    protected $fillable = ['title', 'salary'];
 }
