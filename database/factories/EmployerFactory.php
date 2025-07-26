3e8cfa1ad47b3ac3ce2966edb4668f82275cb6f0
unknown <orifepaul30@gmail.com>
2025-07-25 20:13:37 -0700

    create employer table, migration and model

---
+++ database/factories/EmployerFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployerFactory extends Factory
+{
+    protected $model = Employer::class;
+
+    public function definition(): array
+    {
+        return [
+            'full_name' => $this->faker->company(),
+
+        ];
+    }
+}
